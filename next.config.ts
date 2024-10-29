687c48f81c6117d91eac7f75e064dd3431aa5540
Akshat Jain <akshat00jain@gmail.com>
2024-10-29 18:02:27 +0530

    Commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.vercel.app',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/api/media/**',
+      }
+    ],
+  }
+
 };
 
 export default nextConfig;
