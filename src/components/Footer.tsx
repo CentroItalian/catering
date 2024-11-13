293fabff9eed83dbf846441196ec8b5cf3d3f3dd
Akshat Jain <akshat00jain@gmail.com>
2024-11-13 21:47:38 +0530

    Enhance footer link buttons with hover effects for improved user interaction

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,31 +9,31 @@ const Footer = () => {
       <div className='flex flex-row mb-10 gap-4'>
 
         <Link href={"http://www.yelp.com/biz/mgm-roast-beef-washington"} target='_blank' rel='noopener noreferrer'>
-          <div className={`rounded-lg flex justify-center items-center w-12 h-12 bg-[#FF1A1A]`} title='Yelp'>
+          <div className={`rounded-lg flex justify-center items-center w-12 h-12 bg-[#FF1A1A] ease-in-out duration-300 hover:opacity-80`} title='Yelp'>
             <FaYelp className="text-3xl cursor-pointer" />
           </div>
         </Link>
 
         <Link href={"tel:202-248-0389"} target='_blank' rel='noopener noreferrer'>
-          <div className={`rounded-lg flex justify-center items-center w-12 h-12 bg-[#25D366]`} title='Phone'>
+          <div className={`rounded-lg flex justify-center items-center w-12 h-12 bg-[#25D366] ease-in-out duration-300 hover:opacity-80`} title='Phone'>
             <FaPhone className="text-3xl cursor-pointer" />
           </div>
         </Link>
 
         <Link href={"mailto:orders@mgmroastbeef.com"} target='_blank' rel='noopener noreferrer'>
-          <div className={`rounded-lg flex justify-center items-center w-12 h-12 bg-[#6495ED]`} title='Mail'>
+          <div className={`rounded-lg flex justify-center items-center w-12 h-12 bg-[#6495ED] ease-in-out duration-300 hover:opacity-80`} title='Mail'>
             <MdEmail className="text-3xl cursor-pointer" />
           </div>
         </Link>
 
         <Link href={"https://instagram.com/mgmroastbeef/"} target='_blank' rel='noopener noreferrer'>
-          <div className={`rounded-lg flex justify-center items-center w-12 h-12 bg-[#e1306c]`} title='Instagram'>
+          <div className={`rounded-lg flex justify-center items-center w-12 h-12 bg-[#e1306c] ease-in-out duration-300 hover:opacity-80`} title='Instagram'>
             <FaInstagram className="text-3xl cursor-pointer" />
           </div>
         </Link>
 
         <Link href={"https://maps.app.goo.gl/eJi4B8chyELJaVHJ7"} target='_blank' rel='noopener noreferrer'>
-          <div className={`rounded-lg flex justify-center items-center w-12 h-12 bg-[#E49B0F]`} title='Location'>
+          <div className={`rounded-lg flex justify-center items-center w-12 h-12 bg-[#E49B0F] ease-in-out duration-300 hover:opacity-80 `} title='Location'>
             <FaLocationDot className="text-3xl cursor-pointer" />
           </div>
         </Link>
